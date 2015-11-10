11e80abb9a0ba673225578bf7381961a871222af
nkamau12 <ngumba.kamau@gmail.com>
2015-11-09 23:03:25 -0800

    View Partials and Form Reuse

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use App\Http\Requests;
-use App\Http\Requests\CreateArticleRequest;
+use App\Http\Requests\ArticleRequest;
 
 class ArticlesController extends Controller
 {
@@ -31,11 +31,21 @@ class ArticlesController extends Controller
     }
 
 
-    public function store(CreateArticleRequest $request){
+    public function store(ArticleRequest $request){
 
         Article::create($request->all());
 
         return redirect('articles');
     }
 
+    public function edit($id){
+        $article=Article::findorFail($id);
+        return view('articles.edit',compact('article'));
+    }
+
+    public function update($id, ArticleRequest $request){
+        $article=Article::findOrFail($id);
+        $article->update($request->all());
+        return redirect('articles');
+    }
 }
--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 
 
-class CreateArticleRequest extends Request
+class ArticleRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
