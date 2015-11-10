52b52f606233033253b0b9f5ead55662b7792dfb
nkamau12 <ngumba.kamau@gmail.com>
2015-11-09 19:01:20 -0800

    MVC tutorial

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ArticlesController extends Controller
+{
+    public function index(){
+        $articles= Article::all();
+        return view('articles.index',compact('articles'));
+    }
+    public function show($id){
+        $articles=Article::findorFail($id);
+
+        return view('articles.show',compact('articles'));
+    }
+}
